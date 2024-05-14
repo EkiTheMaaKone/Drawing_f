73e14d867b849891162a4be3c31981021ff1568d
erkka <erkka@DESKTOP-6IN7FIQ>
2024-05-14 11:42:45 +0300

    Add project files.

---
+++ Pi_MonteCarlo.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Drawing_f
+{
+    partial class Pi_MonteCarlo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PiBox = new System.Windows.Forms.PictureBox();
+            this.Iterate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PiBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PiBox
+            // 
+            this.PiBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PiBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PiBox.Location = new System.Drawing.Point(363, 15);
+            this.PiBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PiBox.Name = "PiBox";
+            this.PiBox.Size = new System.Drawing.Size(667, 615);
+            this.PiBox.TabIndex = 0;
+            this.PiBox.TabStop = false;
+            // 
+            // Iterate
+            // 
+            this.Iterate.Location = new System.Drawing.Point(12, 168);
+            this.Iterate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Iterate.Name = "Iterate";
+            this.Iterate.Size = new System.Drawing.Size(125, 23);
+            this.Iterate.TabIndex = 1;
+            this.Iterate.Text = "Generate 1";
+            this.Iterate.UseVisualStyleBackColor = true;
+            this.Iterate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 363);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(557, 654);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 168);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Generate 10";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Generate 100";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(143, 198);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Generate 1000";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(19, 254);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Generate Custom";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(19, 283);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(125, 22);
+            this.numericUpDown1.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AllowDrop = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(329, 100);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Here is a little program for simulating the \r\nvalue of Pi with a Monte Carlo simu" +
+    "lation.\r\n\r\nWhile not very efficient for approximating Pi,\r\nstill a fun little ex" +
+    "ercise.\r\n";
+            // 
+            // Pi_MonteCarlo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1044, 683);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Iterate);
+            this.Controls.Add(this.PiBox);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Pi_MonteCarlo";
+            this.Text = "Approximating Pi using a Monte Carlo Simulation";
+            ((System.ComponentModel.ISupportInitialize)(this.PiBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PiBox;
+        private System.Windows.Forms.Button Iterate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Pi_MonteCarlo.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drawing_f
+{
+    public partial class Pi_MonteCarlo : Form
+    {
+        public Pi_MonteCarlo()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            
+        }
+        public double numPoints = 0;
+        public double inCircle = 0;
+        public Point originPoint = new Point(0, 0);
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            drawPoint(rnd.Next(0,500 * accVar), rnd.Next(0, 500 * accVar));
+            update_labels();
+            drawFirstCircle();
+        }
+        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            drawFirstCircle();
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                drawPoint(rnd.Next(0, 500 * accVar), rnd.Next(0, 500 * accVar));
+            }
+            update_labels();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            drawFirstCircle();
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                drawPoint(rnd.Next(0, 500 * accVar), rnd.Next(0, 500 * accVar));
+            }
+            update_labels();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            drawFirstCircle();
+            Random rnd = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                drawPoint(rnd.Next(0, 500 * accVar), rnd.Next(0, 500 * accVar));
+            }
+            update_labels();
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            drawFirstCircle();
+            Random rnd = new Random();
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                drawPoint(rnd.Next(0, 500*accVar), rnd.Next(0, 500*accVar));
+            }
+            update_labels();
+        }
+        public int accVar=50;
+
+        //Non form functions-------------------------------------------------------------------------
+        private void drawFirstCircle()
+        {
+            Graphics g = Graphics.FromHwnd(PiBox.Handle);
+            Pen penOn = new Pen(Color.Red);
+            //g.DrawEllipse(penOn, -500, -500, 1000, 1000);
+            g.Dispose();
+        }
+        public void drawPoint(int x, int y)
+        {
+            Graphics g = Graphics.FromHwnd(PiBox.Handle);
+            SolidBrush brush = new SolidBrush(Color.Black);
+            Point dPoint = new Point(x/accVar, y/accVar);
+            numPoints++;
+            if (x * x + y * y <= 250000*accVar*accVar)
+            {
+                inCircle++;
+                brush.Color = Color.Cyan;
+            }
+            Rectangle rect = new Rectangle(dPoint, new Size(2, 2));
+            g.FillRectangle(brush, rect);
+            g.Dispose();
+        }
+
+        private void update_labels()
+        {
+            label4.Text = String.Format("{0} out of {1} points are inside the circle",(inCircle).ToString("0"), (numPoints).ToString("0"));
+            //label2.Text = (numPoints).ToString("0.");
+            label1.Text = "Approximation of Pi is Currently:" + ((inCircle / numPoints) * 4).ToString("0.######");
+        }
+
+        
+    }
+}
